4d90e6afb558a9a133c87f4a78d0c30d6a86b1f6
Matheus Lima <matheus_lima789@hotmail.com>
2018-07-10 00:06:05 -0300

    teste splash screen

---
+++ MiAuDota/app/src/main/java/com/laika/miaudota/activities/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.laika.miaudota.activities;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.laika.miaudota.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        }
+    }
+
