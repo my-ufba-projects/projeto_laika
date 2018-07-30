94d6486ef8f68fe76fdd725260b51d9bd8190fc2
Matheus Lima <matheus_lima789@hotmail.com>
2018-07-30 01:40:22 -0300

    Atualizando o ícone

--- MiAuDota/app/build/generated/source/r/debug/android/support/coreutils/R.java
+++ MiAuDota/app/build/generated/source/r/debug/android/support/coreutils/R.java
@@ -50,18 +50,18 @@ public final class R {
         public static final int notification_top_pad_large_text = 0x7f070098;
     }
     public static final class drawable {
-        public static final int notification_action_background = 0x7f080069;
-        public static final int notification_bg = 0x7f08006a;
-        public static final int notification_bg_low = 0x7f08006b;
-        public static final int notification_bg_low_normal = 0x7f08006c;
-        public static final int notification_bg_low_pressed = 0x7f08006d;
-        public static final int notification_bg_normal = 0x7f08006e;
-        public static final int notification_bg_normal_pressed = 0x7f08006f;
-        public static final int notification_icon_background = 0x7f080070;
-        public static final int notification_template_icon_bg = 0x7f080071;
-        public static final int notification_template_icon_low_bg = 0x7f080072;
-        public static final int notification_tile_bg = 0x7f080073;
-        public static final int notify_panel_notification_icon_bg = 0x7f080074;
+        public static final int notification_action_background = 0x7f08006a;
+        public static final int notification_bg = 0x7f08006b;
+        public static final int notification_bg_low = 0x7f08006c;
+        public static final int notification_bg_low_normal = 0x7f08006d;
+        public static final int notification_bg_low_pressed = 0x7f08006e;
+        public static final int notification_bg_normal = 0x7f08006f;
+        public static final int notification_bg_normal_pressed = 0x7f080070;
+        public static final int notification_icon_background = 0x7f080071;
+        public static final int notification_template_icon_bg = 0x7f080072;
+        public static final int notification_template_icon_low_bg = 0x7f080073;
+        public static final int notification_tile_bg = 0x7f080074;
+        public static final int notify_panel_notification_icon_bg = 0x7f080075;
     }
     public static final class id {
         public static final int action_container = 0x7f090011;
--- MiAuDota/app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
+++ MiAuDota/app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
@@ -578,20 +578,20 @@ public final class R {
         public static final int abc_textfield_search_default_mtrl_alpha = 0x7f080057;
         public static final int abc_textfield_search_material = 0x7f080058;
         public static final int abc_vector_test = 0x7f080059;
-        public static final int notification_action_background = 0x7f080069;
-        public static final int notification_bg = 0x7f08006a;
-        public static final int notification_bg_low = 0x7f08006b;
-        public static final int notification_bg_low_normal = 0x7f08006c;
-        public static final int notification_bg_low_pressed = 0x7f08006d;
-        public static final int notification_bg_normal = 0x7f08006e;
-        public static final int notification_bg_normal_pressed = 0x7f08006f;
-        public static final int notification_icon_background = 0x7f080070;
-        public static final int notification_template_icon_bg = 0x7f080071;
-        public static final int notification_template_icon_low_bg = 0x7f080072;
-        public static final int notification_tile_bg = 0x7f080073;
-        public static final int notify_panel_notification_icon_bg = 0x7f080074;
-        public static final int tooltip_frame_dark = 0x7f08007a;
-        public static final int tooltip_frame_light = 0x7f08007b;
+        public static final int notification_action_background = 0x7f08006a;
+        public static final int notification_bg = 0x7f08006b;
+        public static final int notification_bg_low = 0x7f08006c;
+        public static final int notification_bg_low_normal = 0x7f08006d;
+        public static final int notification_bg_low_pressed = 0x7f08006e;
+        public static final int notification_bg_normal = 0x7f08006f;
+        public static final int notification_bg_normal_pressed = 0x7f080070;
+        public static final int notification_icon_background = 0x7f080071;
+        public static final int notification_template_icon_bg = 0x7f080072;
+        public static final int notification_template_icon_low_bg = 0x7f080073;
+        public static final int notification_tile_bg = 0x7f080074;
+        public static final int notify_panel_notification_icon_bg = 0x7f080075;
+        public static final int tooltip_frame_dark = 0x7f08007b;
+        public static final int tooltip_frame_light = 0x7f08007c;
     }
     public static final class id {
         public static final int action_bar = 0x7f09000a;
